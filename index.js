f3b83011b55c3342324f33e005cc7027382ce3f0
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-08-29 10:09:21 +0600

    add nevbar

---
+++ index.js
@@ -0,0 +1,28 @@
+var getNavElem = document.getElementById("nevbar");
+
+getNavElem.innerHTML = `
+<div id="nav-header">
+<img class="logo" src="images/Health-1.png" alt="logo" height="64px" width="64px">
+
+<nav>
+    <ul class="nav_links">
+
+        <li>
+            <a href="#">Services</a>
+        </li>
+        <li>
+            <a href="#">Projects</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+
+    </ul>
+</nav>
+
+<a href="#" class="cta">
+    <button>Contact</button>
+</a>
+
+</div>
+`;
---
+++ nevbar/nevbar.js
@@ -0,0 +1,3 @@
+var elem = document.getElementsByTagName("header");
+
+console.log(elem);
